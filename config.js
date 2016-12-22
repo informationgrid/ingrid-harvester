e1882bd88b7cc7de357399b4fefc6fabcba8f448
jm <jm@wemove.com>
2016-12-22 18:51:07 +0100

    - add data.xslx as data file to config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
             index: 'excel',
             indexType: 'base',
             alias: 'mcloud',
-            filePath: '', // xlsx-file for import
+            filePath: './data.xslx', // xlsx-file for import
             mapper: [ excelDefaultMapper, excelIdfMapper ],
             includeTimestamp: true
         }
