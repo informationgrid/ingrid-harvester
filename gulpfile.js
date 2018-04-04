7a938707e871f4ec39f7a70fa5a58c2257d10c5f
Andre Wallat <andre.wallat@wemove.com>
2018-04-04 18:11:51 +0200

    - bundle application

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
+var browserify = require('browserify');
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
 // const concat = require('gulp-concat');
 
 gulp.task('default', () => {
-    return gulp.src('server/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['es2017']
-        }))
-        // .pipe(concat('all.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
+
+    // set up the browserify instance on a task basis
+    var b = browserify({
+        entries: './server/server.js',
+        node: true
+    });
+
+    return b.bundle()
+        .pipe(source('app.js'))
+        .pipe(buffer())
+        .pipe(gulp.dest('./dist/'));
 });
