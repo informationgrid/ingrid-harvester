65679c2f1276b3143544b41503af7dc24ad2b241
Vikram Notay <vikram.notay@wemove.com>
2018-06-04 14:56:09 +0200

    Update CKAN importer for new index profile
    Update importer for importing from CKAN datasources according to the new
    elasticsearch profile for mcloud.

--- server/ckan/importer.js
+++ server/ckan/importer.js
@@ -4,6 +4,7 @@ let request = require( 'request-promise' ),
     async = require( 'async' ),
     log = require( 'log4js' ).getLogger( __filename ),
     ElasticSearchUtils = require( './../elastic-utils' ),
+    settings = require('../elastic.settings.js'),
     mapping = require( '../elastic.mapping.js' );
 
 class GovDataImporter {
@@ -16,13 +17,13 @@ class GovDataImporter {
         this.settings = settings;
         this.elastic = new ElasticSearchUtils(settings);
 
-        this.options_url_ids = {
-            uri: settings.urlSearch,
-            headers: {'User-Agent': 'Request-Promise'},
-            json: true
-        };
-        this.options_url_dataset = {
-            uri: settings.urlData,
+        this.options_package_search = {
+            uri: settings.ckanBaseUrl + "/api/3/action/package_search", // See http://docs.ckan.org/en/ckan-2.7.3/api/
+            qs: {
+                sort: "id asc",
+                start: 0,
+                rows: 100
+            },
             headers: {'User-Agent': 'Request-Promise'},
             json: true
         };
@@ -33,50 +34,128 @@ class GovDataImporter {
      * @param {string} id
      * @param {function} callback
      */
-    async importDataset(id, callback) {
-        let options = Object.assign( {}, this.options_url_dataset );
-        options.uri += id;
+    async importDataset(source, callback) {
         try {
-            let json = await request.get( options );
-            let doc = {};
-            log.debug( 'dataset: ' + id );
+            log.debug("Processing dataset: " + source.name)
 
-            // process all tasks in the mapping pipeline
-            this.settings.mapper.forEach( mapper => mapper.run( json, doc ) );
+            let target = {};
+            target.id = source.id;
+            target.name = source.name;
+            target.title = source.title;
+            target.description = source.notes;
+            target.theme = ['http://publications.europa.eu/resource/authority/data-theme/TRAN']; // see https://joinup.ec.europa.eu/release/dcat-ap-how-use-mdr-data-themes-vocabulary
+            target.issued = source.metadata_created;
+            target.modified = source.metadata_modified;
+            target.accrualPeriodicity = source.update_cycle;
 
-            this.elastic.addDocToBulk( doc, doc.id );
+            // Keywords
+            if (source.tags !== null) {
+                target.keywords = [];
+                source.tags.forEach(tag => {
+                    if (tag.display_name !== null) {
+                        target.keywords.push(tag.display_name);
+                    }
+                });
+            }
+
+            // Creator
+            if (source.author !== null || source.author_email !== null) {
+                target.creator = {
+                    name: source.author,
+                    mbox: source.author_email
+                };
+            }
+
+            // Organisation
+            if (source.organization !== null) {
+                target.publisher = {};
+                if (source.organization.title !== null) {
+                    target.publisher.organization = [source.organization.title];
+                    target.publisher.homepage = [];
+                }
+            }
+
+            // Resources/Distributions
+            if (source.resources !== null) {
+                target.distribution = [];
+                source.resources.forEach(res => {
+                    let dist = {
+                        id: res.id,
+                        title: res.name,
+                        description: res.description,
+                        accessURL: res.url,
+                        format: res.format,
+                        issued: res.created,
+                        modified: res.last_modified,
+                        byteSize: res.size
+                    };
+                    target.distribution.push(dist);
+                });
+            }
+
+            // Extras
+            target.extras = {
+                // TODO subcategories (=mcloud categories)
+                license_id: source.license_id,
+                license_title: source.license_title,
+                license_url: source.license_url
+            };
+
+            // Groups
+            if (source.groups !== null) {
+                target.extras.groups = [];
+                source.groups.forEach(group => {
+                    target.extras.groups.push(group.display_name);
+                });
+            }
+
+            // Metadata dates
+            let now = new Date(Date.now());
+            target.extras.metadata = {
+                modified: now,
+                harvested: now
+            };
+
+            this.elastic.addDocToBulk(target, target.id);
 
             // signal finished operation so that the next asynchronous task can run
             callback();
 
-        } catch (err) {
-            log.error( 'Error: ' + err.statusCode );
+        } catch (e) {
+            log.error("Error: " + e);
         }
     }
 
     async run() {
         try {
 
-            // prepare index with correct mapping
-            this.elastic.prepareIndex( mapping );
-
-            // get all IDs of the documents to be fetched
-            let json = await request.get( this.options_url_ids );
-            let ids = json[ 'results' ];
-            log.debug( 'result:' + JSON.stringify( json ) );
-
-            // create the queue and fill it with all the tasks
-            // it's important to call method with the correct scope here, so that class properties can be accessed!
             let self = this;
-            let queue = async.queue( function() { self.importDataset.call(self, ...arguments); }, 10 );
-            ids.forEach( id => queue.push( id ) );
+            let queue = async.queue(function () {
+                self.importDataset.call(self, ...arguments);
+            }, 10);
 
-            // when queue is empty then send the last data form bulk and close the client
             queue.drain = () => {
                 log.info( 'queue is empty' );
-                // send rest of the data to elasticsearch and close the client
-                this.elastic.sendBulkData( true );
+                // Prepare the index, send the data to elasticsearch and close the client
+                this.elastic.prepareIndex(mapping, settings)
+                    .then(() => this.elastic.sendBulkData(false)
+                        .then(() => this.elastic.finishIndex())
+                    );
             };
+
+            // Fetch datasets 'qs.rows' at a time
+            while(true) {
+                let json = await request.get(this.options_package_search);
+                let results = json.result.results;
+
+                results.forEach(dataset => queue.push(dataset));
+
+                if (results.length < 1) {
+                    break;
+                } else {
+                    this.options_package_search.qs.start += this.options_package_search.qs.rows;
+                }
+            }
 
         } catch (err) {
             log.error( 'error:', err );
