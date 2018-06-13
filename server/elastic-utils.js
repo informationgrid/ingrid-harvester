88910a2e2f803b4407a9ff7c28799d7f0fc22228
Vikram Notay <vikram.notay@wemove.com>
2018-06-13 12:55:04 +0200

    Search duplicates before adding document to index
    Before adding a document to the elasticsearch index, check whether any
    duplicates are present in the existing index. If that is the case, then
    skip indexing the duplicate document.

--- server/elastic-utils.js
+++ server/elastic-utils.js
@@ -183,15 +183,14 @@ class ElasticSearchUtils {
             }, (err) => {
                 if (err) {
                     log.error('Error occurred during bulk index', err);
-                    reject();
+                    reject(err);
                     return;
                 }
                 if (closeAfterBulk) {
                     this.client.close();
                 }
                 resolve();
-            })
-            .then(() => resolve());
+            });
         });
     }
 
@@ -202,18 +201,28 @@ class ElasticSearchUtils {
      * @param {string|number} id
      */
     addDocToBulk(doc, id) {
-        this._bulkData.push({
-            index: {
-                _id: id
-            }
-        });
-        this._bulkData.push(doc);
+        return new Promise((resolve, reject) => {
+            this.newerDuplicatesFor(doc, id)
+                .then(result => {
+                    if (result.length > 0) {
+                        log.warn(`Skipping document with id '${doc.id}' since it is a duplicate of document with id '${result[0]._source.id}' from index '${result[0]._index}'`);
+                    } else {
+                        this._bulkData.push({
+                            index: {
+                                _id: id
+                            }
+                        });
+                        this._bulkData.push(doc);
 
-        // send data to elasticsearch if limit is reached
-        // TODO: don't use document size but bytes instead
-        if (this._bulkData.length > this.maxBulkSize) {
-            return this.sendBulkData();
-        }
+                        // send data to elasticsearch if limit is reached
+                        // TODO: don't use document size but bytes instead
+                        if (this._bulkData.length > this.maxBulkSize) {
+                            this.sendBulkData();
+                        }
+                    }
+                })
+                .catch(err => reject(err));
+        });
     }
 
     /**
@@ -269,8 +278,92 @@ class ElasticSearchUtils {
             });
 
             response.then(res => resolve(res))
-                .catch(err => reject(err));
+                .catch(err => {
+                    //log.warn("Error searching by id", err);
+                    resolve({ hits: { total: 0}})
+                });
         });
+    }
+
+    /**
+     * Searches duplicates with newer modification dates for the given document
+     * and the given id.
+     *
+     * Indexed documents with the following properties are returned:
+     * - modified date is strictly newer than the given document's modification
+     *   date, AND
+     * - Any of the following is true:
+     *     - _id is equal to the document's _id or id field
+     *     - Given id is equal to the the document's _id or id field
+     *     - All of the following are true:
+     *         - Titles with 3 words or less match exactly OR longer titles have
+     *           at least 75% words matching
+     *         - The download urls match exactly
+     *
+     *
+     * @param doc document for which to search duplicates
+     * @param id value to be assigned to the _id field of the document above
+     * @returns {*|Promise} array of duplicate documents, empty array if no
+     * duplicates were found
+     */
+    newerDuplicatesFor(doc, id) {
+        let harvestedId = doc.id;
+        let source = doc.extras.metadata.source;
+        let dt = doc.modified;
+        let title = doc.title;
+
+        let urls = [];
+        doc.distribution.forEach(dist => {
+            if (dist.accessURL) {
+                urls.push(dist.accessURL);
+            }
+        });
+
+        return new Promise((resolve, reject) => {
+            const response = this.client.search({
+                index: this.settings.alias,
+                body: {
+                    query: {
+                        filtered: {
+                            filter: {
+                                bool: {
+                                    must: { "range": { "modified": { "gt": dt } } },
+                                    should: [
+                                        { term: { "id": id }},
+                                        { term: { "id": harvestedId }},
+                                        { term: { "_id": id }},
+                                        { term: { "_id": harvestedId }},
+                                        {
+                                            query: {
+                                                bool: {
+                                                    must: [
+                                                        {
+                                                            match: {
+                                                                "title": {
+                                                                    "query": title,
+                                                                    "minimum_should_match": "3<75%"
+                                                                }
+                                                            }
+                                                        },
+                                                        { terms: { "dataset.accessURL": urls } }
+                                                    ]
+                                                }
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+            response.then(results => resolve(results.hits.hits))
+                .catch(err => {
+                    //log.warn("Error searching duplicates", err);
+                    resolve([]);
+                });
+        })
     }
 }
 
