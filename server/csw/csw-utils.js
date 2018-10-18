6d15ad0fbfd594d1ee3bef056a6c84849ff1f3c1
Vikram Notay <vikram.notay@wemove.com>
2018-10-18 09:54:40 +0200

    Fix xpath query for (Date or DateTime) selection
    Fix xpath query that select either Date or DateTime elements from a
    node.

--- server/csw/csw-utils.js
+++ server/csw/csw-utils.js
@@ -129,7 +129,7 @@ class CswUtils {
         target.keywords = keywords;
         target.theme = ['http://publications.europa.eu/resource/authority/data-theme/TRAN']; // see https://joinup.ec.europa.eu/release/dcat-ap-how-use-mdr-data-themes-vocabulary
 
-        let created = select('./gmd:dateStamp/gco:Date|gco:DateTime', record, true).textContent;
+        let created = select('./gmd:dateStamp/gco:Date|./gmd:dateStamp/gco:DateTime', record, true).textContent;
         target.issued = created;
         this.extractModifiedDate(target, idInfo);
 
@@ -317,7 +317,9 @@ class CswUtils {
     }
 
     extractModifiedDate(target, idInfo) {
-        let dt = select('./*/gmd:citation/*/gmd:date/*[./gmd:dateType/gmd:CI_DateTypeCode/@codeListValue="revision"]/gmd:date/gco:Date|gco:DateTime', idInfo);
+        let dateXpath = './*/gmd:citation/*/gmd:date/*[./gmd:dateType/gmd:CI_DateTypeCode/@codeListValue="revision"]/gmd:date';
+        dateXpath = `${dateXpath}/gco:Date|${dateXpath}/gco:DateTime`;
+        let dt = select(dateXpath, idInfo);
         // Multiple dates may be defined (creation, publication, revision).
         // Just use the first found revision date
         if (dt.length > 0) target.modified = dt[0].textContent;
