587005fb5f9c9a563808dbf555e8518fe481b91d
André Wallat <andre.wallat@wemove.com>
2019-09-10 11:10:22 +0200

    Fix compilation error

--- client/src/app/harvester/dialog-log/dialog-log.component.ts
+++ client/src/app/harvester/dialog-log/dialog-log.component.ts
@@ -1,5 +1,5 @@
-import {Component, Inject, Input, OnInit} from '@angular/core';
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 import {ImportLogMessage} from "../../../../../server/app/model/import.result";
 
 @Component({
@@ -11,7 +11,7 @@ export class DialogLogComponent implements OnInit {
 
   logText: string;
   appErrors: string[] = [];
-  private elasticSearchErrors: string[] = [];
+  elasticSearchErrors: string[] = [];
   appWarnings: string[][] = [];
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
