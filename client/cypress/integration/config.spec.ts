a959def704934e6a38e698c24f4ab9b8751d4e69
Nizar Bikti <nizar.bikti@gmail.com>
2019-11-08 12:56:35 +0100

    Add after hook, clean up code and activate pending test

--- client/cypress/integration/config.spec.ts
+++ client/cypress/integration/config.spec.ts
@@ -12,77 +12,89 @@ function pressResetButton() {
   cy.get('[data-test=reset]').contains('Zurücksetzen').click();
 }
 
+function sendConfigToApi() {
+  cy.request({
+    method: 'POST',
+    url: 'rest/api/config/general',
+    body: {"elasticSearchUrl":"http://localhost:9200",
+      "alias":"mcloud",
+      "proxy":"",
+      "sessionSecret":"mysecretkey"}
+  });
+  }
+
 describe('configuration tab operations', () => {
   beforeEach(() => {
     cy.apiLogin('admin', 'admin');
     cy.goToConfig();
+  });
 
-    //clean up state
-    // TODO: use API request to set configuration in backend directly (much faster!)
-    // TODO: should be executed in afterEach to make sure that state is clean after last test
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type('http://localhost:9200');
-    cy.get('[formcontrolname=alias]').clear().type('mcloud');
-    cy.get('[formcontrolname=proxy]').clear();
-    pressSaveButton();
+  /**
+   * clean up after the tests'
+   */
+  afterEach(() => {
+    sendConfigToApi();
   });
 
   it('should update the elastic search-url, the alias and proxy values, save and check the saved data', () => {
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type('http://localhost:9209');
-    cy.get('[formcontrolname=alias]').clear().type('eman-saila');
-    cy.get('[formcontrolname=proxy]').clear().type('yxorp');
+    cy.get('[name="elasticsearchUrl"]').clear().type('http://localhost:9209');
+    cy.get('[name="alias"]').clear().type('eman-saila');
+    cy.get('[name="proxy"]').clear().type('yxorp');
 
     pressSaveButton();
     cy.reload();
 
     //checks
-    cy.get('[formcontrolname=elasticSearchUrl]').should('have.value', 'http://localhost:9209');
-    cy.get('[formcontrolname=alias]').should('have.value', 'eman-saila');
-    cy.get('[formcontrolname=proxy]').should('have.value', 'yxorp');
+    cy.get('[name="elasticsearchUrl"]').should('have.value', 'http://localhost:9209');
+    cy.get('[name="alias"]').should('have.value', 'eman-saila');
+    cy.get('[name="proxy"]').should('have.value', 'yxorp');
+
+    sendConfigToApi();
 
   });
 
   it('should update elastic search-url, alias and proxy, reset to default and check the reset is successful', () => {
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type('http://localhost:92000000');
-    cy.get('[formcontrolname=alias]').clear().type('eman-saila');
-    cy.get('[formcontrolname=proxy]').clear().type('yxorp');
+    cy.get('[name="elasticsearchUrl"]').clear().type('http://localhost:92000000');
+    cy.get('[name="alias"]').clear().type('eman-saila');
+    cy.get('[name="proxy"]').clear().type('yxorp');
 
     pressResetButton();
 
     //checks
-    cy.get('[formcontrolname=elasticSearchUrl]').should('have.value', 'http://localhost:9200');
-    cy.get('[formcontrolname=alias]').should('have.value', 'mcloud');
-    cy.get('[formcontrolname=proxy]').should('have.value', '');
+    cy.get('[name="elasticsearchUrl"]').should('have.value', 'http://localhost:9200');
+    cy.get('[name="alias"]').should('have.value', 'mcloud');
+    cy.get('[name="proxy"]').should('have.value', '');
   });
 
   it('should check that the save button is disabled if only spaces are inserted [INPUT CONTROL]', () => {
     //no value
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type(' ');
-    cy.get('[data-test=save]').should('be.disabled');
+    cy.get('[name="elasticsearchUrl"]').clear().type(' ');
+    cy.get('[data-test="save"]').should('be.disabled');
 
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type('http://localhost:9200');
-    cy.get('[formcontrolname=alias]').clear().type(' ');
-    cy.get('[data-test=save]').should('be.disabled');
+    cy.get('[name="elasticsearchUrl"]').clear().type('http://localhost:9200');
+    cy.get('[name="alias"]').clear().type(' ');
+    cy.get('[data-test="save"]').should('be.disabled');
 
-    cy.get('[formcontrolname=alias]').clear().type('mcloud');
-    cy.get('[data-test=save]').should('be.enabled');
+    cy.get('[name="alias"]').clear().type('mcloud');
+    cy.get('[data-test="save"]').should('be.enabled');
   });
 
   it('should check that the save button is disabled if wrong port values are inserted [INPUT CONTROL]', () => {
     //no value
-    cy.get('[formcontrolname=elasticSearchUrl]').clear();
-    cy.get('[data-test=save]').should('be.disabled');
+    cy.get('[name="elasticsearchUrl"]').clear();
+    cy.get('[data-test="save"]').should('be.disabled');
 
     //value is too big
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type('http://localhost:92000000');
-    cy.get('[data-test=save]').should('be.disabled');
+    cy.get('[name="elasticsearchUrl"]').clear().type('http://localhost:92000000');
+    cy.get('[data-test="save"]').should('be.disabled');
 
     //value is NaN
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type('http://localhost:porttout');
-    cy.get('[data-test=save]').should('be.disabled');
+    cy.get('[name="elasticsearchUrl"]').clear().type('http://localhost:porttout');
+    cy.get('[data-test="save"]').should('be.disabled');
 
     //value is negative
-    cy.get('[formcontrolname=elasticSearchUrl]').clear().type('http://localhost:-42');
-    cy.get('[data-test=save]').should('be.disabled');
+    cy.get('[name="elasticsearchUrl"]').clear().type('http://localhost:-42');
+    cy.get('[data-test="save"]').should('be.disabled');
   });
 
   it('should export the harvester configuration if the right request in made', () => {
@@ -97,7 +109,11 @@ describe('configuration tab operations', () => {
     });
   });
 
-  xit('should export the harvester configuration if the button is pressed', () => {
+  /**
+   * In Electron a save dialog is shown which prevents further test execution
+   * TODO: Wait for issue: https://github.com/cypress-io/cypress/issues/949
+   */
+  it('should export the harvester configuration if the button is pressed', () => {
     cy.server();
     cy.route('GET', 'http://192.168.0.228/importer/rest/api/harvester').as('download');
     cy.goToConfig();
