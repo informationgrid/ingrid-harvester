90afdbdb60976424d119a00b7841e49164555d4c
Jan Buecher <51136390+John-Books@users.noreply.github.com>
2020-03-17 16:14:39 +0100

    mail notification for data loss by percentage(#2384)

--- server/app/sockets/import.socket.service.ts
+++ server/app/sockets/import.socket.service.ts
@@ -68,8 +68,8 @@ export class ImportSocketService {
                         // when less results send mail
                         let importedLastRun = (summaryLastRun) ? summaryLastRun.summary.numDocs - summaryLastRun.summary.skippedDocs.length : 0;
                         let imported = importer.getSummary().numDocs - importer.getSummary().skippedDocs.length;
-                        let maxDiff = (configGeneral.maxDiff) ? configGeneral.maxDiff : 0;
-                        if ((importedLastRun - imported > maxDiff) || (imported === 0)) {
+                        let maxDiff = (configGeneral.maxDiff) ? configGeneral.maxDiff : 10;
+                        if ((importedLastRun - (importedLastRun*maxDiff/100) >= imported) || (imported === 0)) {
                             let subject: string;
                             if (imported === 0)
                                 subject = `[mCloud] Importer "${configData.description}" ohne Ergebnisse!`;
