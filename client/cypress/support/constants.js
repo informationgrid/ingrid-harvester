7e3b8f73abeb251ee1c54568b6c5ccbe465e7622
Majid Ercan <majid.ercan@wemove.com>
2020-07-14 16:47:12 +0200

    Fix tests

--- client/cypress/support/constants.js
+++ client/cypress/support/constants.js
@@ -6,10 +6,13 @@ class Constants {
   CKAN_DB_ID = '1';
   CKAN_RNV_ID = '2';
   CKAN_TEST_ID = '3';
+  CKAN_DBD_ID = '6';
+  CKAN_GEONET_ID = '32';
 
   CSW_CODEDE_ID = '4';
   CSW_GEOSERVICE_ID = '5';
   CSW_TEST_ID = '6';
+  CSW_WSV_ID = '12';
 
   EXCEL_MCLOUD_ID = '7';
   EXCEL_TEST_ID = '8';
