1f08c4c7f48cc5d2d2f6b07fe6b4126e828bb926
Jan Buecher <51136390+John-Books@users.noreply.github.com>
2021-02-02 17:13:19 +0100

    Correct Importer-Tests

--- server/test/import-excel.spec.ts
+++ server/test/import-excel.spec.ts
@@ -62,16 +62,16 @@ describe('Import Excel', function () {
         mapper.columnValues = [];
 
         mapper.columnValues[22] = "12.08.2018";
-        chai.expect(mapper.getTemporal()).to.deep.equal({start: new Date("08/12/2018"), end: new Date("08/12/2018")});
+        chai.expect(mapper.getTemporal()).to.deep.equal({gte: new Date("08/12/2018"), lte: new Date("08/12/2018")});
 
         //mapper.columnValues[22] = "heute";
         //chai.expect(mapper.getTemporal().custom).to.equal("heute");
 
         mapper.columnValues[22] = "12.08.2018 - 25.09.2018";
-        chai.expect(mapper.getTemporal()).to.deep.equal({start: new Date("08/12/2018"), end: new Date("09/25/2018")});
+        chai.expect(mapper.getTemporal()).to.deep.equal({gte: new Date("08/12/2018"), lte: new Date("09/25/2018")});
 
         mapper.columnValues[22] = "12.08.2018 - heute";
-        chai.expect(mapper.getTemporal()).to.deep.equal({custom: "12.08.2018 - heute"});
+        chai.expect(mapper.getTemporal()).to.deep.equal({gte: new Date("08/12/2018"), lte: new Date().setTime(0)});
 
         //mapper.columnValues[22] = "2017";
         //chai.expect(mapper.getTemporal()).to.deep.equal({custom: "2017"});
