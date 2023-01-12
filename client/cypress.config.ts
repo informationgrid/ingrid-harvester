28055b970043ce99ba29f639c569f17b2d2dcab3
Nizar Bikti <nizar.bikti@gmail.com>
2023-01-12 16:23:37 +0100

    Update cypress to v12.3.0
    (cherry picked from commit 15608670c86705b9bc222994094c8297642dd448)

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,30 +1,35 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  reporter: 'mochawesome',
+  reporter: "mochawesome",
+
   reporterOptions: {
-    reportDir: 'cypress/mochawesome-reports',
+    reportDir: "cypress/mochawesome-reports",
     overwrite: false,
     html: false,
     json: true,
-    timestamp: 'yyyymmdd_HHMMss',
+    timestamp: "yyyymmdd_HHMMss",
   },
-  screenshotsFolder: 'cypress/mochawesome-reports/assets',
-  videosFolder: 'cypress/mochawesome-reports/assets',
+
+  screenshotsFolder: "cypress/mochawesome-reports/assets",
+  videosFolder: "cypress/mochawesome-reports/assets",
   videoUploadOnPasses: false,
-  downloadsFolder: 'cypress/downloads',
+  downloadsFolder: "cypress/downloads",
+
   retries: {
     runMode: 2,
     openMode: 0,
   },
+
   watchForFileChanges: false,
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://192.168.0.228/importer/',
-    specPattern: 'cypress/e2e/**/*.ts',
+    baseUrl: "http://192.168.0.228/importer/",
+    specPattern: "cypress/e2e/**/*.ts",
   },
 });
