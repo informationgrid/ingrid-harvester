860875be8c81c6deb50669732ab3fcddbc7cd78c
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2023-01-21 00:45:43 +0100

    Refactor services as ElasticSearchUtils extensions

--- server/app/services/statistic/IndexCheckService.ts
+++ server/app/services/statistic/IndexCheckService.ts
@@ -22,281 +22,65 @@
  */
 
 import {Service} from '@tsed/di';
-import {ImportSocketService} from '../../sockets/import.socket.service';
 import {ElasticSearchUtils} from "../../utils/elastic.utils";
 import {ConfigService} from "../config/ConfigService";
-import {ElasticSettings} from "../../utils/elastic.setting";
 import {Summary} from "../../model/summary";
 import {now} from "moment";
 import {elasticsearchMapping} from "../../statistic/url_check.mapping";
 import {elasticsearchSettings} from "../../statistic/url_check.settings";
-import {BulkResponse} from "../../statistic/statistic.utils";
-import { Client } from '@elastic/elasticsearch';
+import { ElasticQueries } from '../../utils/elastic.queries';
 
 let log = require('log4js').getLogger(__filename);
 
 require('url').URL;
 
 @Service()
-export class IndexCheckService {
-    private elasticUtils: ElasticSearchUtils;
-    private _bulkData: any[];
-    private generalSettings;
-    private settings: ElasticSettings;
-    private indexName: string;
+export class IndexCheckService extends ElasticSearchUtils {
 
-    private client: Client;
-
-    constructor(private socketService: ImportSocketService) {
-        this.generalSettings = ConfigService.getGeneralSettings();
-        this.settings = {
-            elasticSearchUrl: this.generalSettings.elasticSearchUrl,
-            elasticSearchPassword: this.generalSettings.elasticSearchPassword,
-            alias: this.generalSettings.alias,
-            includeTimestamp: true,
-            index: ''
+    constructor() {
+        let generalSettings = ConfigService.getGeneralSettings();
+        let settings = {
+            elasticSearchUrl: generalSettings.elasticSearchUrl,
+            elasticSearchPassword: generalSettings.elasticSearchPassword,
+            alias: generalSettings.alias,
+            includeTimestamp: false,
+            index: 'index_check_history'
         };
-
-        // the elasticsearch client for accessing the cluster
-        this.client = new Client({
-            node: this.settings.elasticSearchUrl,
-            auth: {
-                username: 'elastic',
-                password: this.settings.elasticSearchPassword
-            },
-            requestTimeout: 30000
-        });
-
         // @ts-ignore
         const summary: Summary = {};
-        this.elasticUtils = new ElasticSearchUtils(this.settings, summary);
-        this._bulkData = [];
-
-        this.indexName = 'index_check_history';
+        super(settings, summary);
     }
 
-    async getHistory(){
-        let history = await this.elasticUtils.getIndexCheckHistory();
+    async getHistory() {
+        let result = await this.client.search({
+            index: [this.indexName],
+            body: ElasticQueries.getIndexCheckHistory(),
+            size: 30
+        });
         return {
-            history: history
+            history: result.hits.hits.map(entry => entry._source)
         }
     }
 
     async start() {
         log.info('IndexCheck started!')
         let start = now();
-        let facetsByAttribution = await this.elasticUtils.getFacetsByAttribution();
+        let facetsByAttribution = await this.getFacetsByAttribution();
         this.saveResult(facetsByAttribution, new Date(start));
     }
 
     async saveResult(result, timestamp) {
-            this.addDocToBulk({
+        try {
+            await this.addDocToBulk({
                 timestamp: timestamp,
                 attributions: result
             }, timestamp.toISOString());
-
-            await this.prepareIndex(elasticsearchMapping, elasticsearchSettings)
-                .then(() => this.finishIndex())
-                .catch(err => {
-                    let message = 'Error occurred creating UrlCheck index';
-                    log.error(message, err);
-                });
-    }
-
-    /**
-     * Add a document to the bulk array which will be sent to the elasticsearch node
-     * if a certain limit {{maxBulkSize}} is reached.
-     * @param doc
-     * @param {string|number} id
-     */
-    addDocToBulk(doc, id): Promise<BulkResponse> {
-        this._bulkData.push({
-            index: {
-                _id: id
-            }
-        });
-        this._bulkData.push(doc);
-
-        return new Promise(resolve => resolve({
-            queued: true
-        }));
-    }
-
-    /**
-     *
-     * @param mapping
-     * @param settings
-     */
-    prepareIndex(mapping, settings) {
-        let idxSettings = {
-            number_of_shards: this.generalSettings.numberOfShards,
-            number_of_replicas: this.generalSettings.numberOfReplicas
+            await this.prepareIndex(elasticsearchMapping, elasticsearchSettings, true);
+            await this.finishIndex(false);
         }
-        return new Promise<void>((resolve, reject) => {
-            this.isIndexPresent(this.indexName).then(isPresent => {
-
-                if (!isPresent) {
-                    this.client.indices.create({index: this.indexName, wait_for_active_shards: 1, settings: idxSettings})
-                        .then(() => this.addMapping(this.indexName, this.settings.indexType, mapping, settings, resolve, reject))
-                        .catch(err => {
-                            let message = 'Error occurred creating UrlCheck index';
-                            log.error(message, err);
-                            reject(message);
-                        });
-                } else {
-                    this.client.indices.open({index: this.indexName, wait_for_active_shards: 1}).catch(err => {
-                        let message = 'Error occurred creating UrlCheck index';
-                        log.error(message, err);
-                        reject(message);
-                    });
-                    resolve();
-                }
-            });
-        });
-    }
-
-    finishIndex() {
-        return this.client.cluster.health({wait_for_status: 'yellow'})
-            .then(() =>
-                this.sendBulkData(false))
-            .then(() => {
-                log.info('Successfully added UrlCheck data into index: ' + this.indexName);
-            })
-            .catch(err => log.error('Error finishing index', err));
-    }
-
-    isIndexPresent(index: string) {
-        let result: boolean;
-        return this.client.cat.indices({
-            h: ['index'],
-            format: 'json'
-        }).then(response => {
-            return response
-                .some(json => {
-                    return index === json.index;
-                })
-        }).catch(e => log.error(e));
-
-    }
-
-    /**
-     * Add the specified mapping to an index and type.
-     *
-     * @param {string} index
-     * @param {string} type
-     * @param {object} mapping
-     * @param {object} settings
-     * @param callback
-     * @param errorCallback
-     */
-    addMapping(index, type, mapping, settings, callback, errorCallback) {
-
-        // set settings
-        const handleSettings = () => {
-            this.client.indices.putSettings({
-                index: index,
-                body: settings
-            }).catch(err => {
-                if (err) {
-                    log.error('Error occurred adding settings', err);
-                    errorCallback(err);
-                } else handleMapping(); //this.client.indices.open({ index: index });
-            });
-        };
-
-        // set mapping
-        const handleMapping = () => {
-            this.client.indices.putMapping({
-                index: index,
-                // type: type || 'base',
-                body: mapping
-            }).catch(err => {
-                if (err) {
-                    log.error('Error occurred adding mapping', err);
-                    errorCallback('Mapping error');
-                } else this.client.indices.open({index: index}).catch(errOpen => {
-                    if (errOpen) {
-                        log.error('Error opening index', errOpen);
-                    }
-                    callback();
-                });
-            });
-        };
-
-        // in order to update settings the index has to be closed
-        const handleClose = () => {
-            this.client.cluster.health({wait_for_status: 'yellow'})
-                .then(() => this.client.indices.close({index: index}).then(handleSettings))
-                .catch(() => {
-                    log.error('Cluster state did not become yellow');
-                    errorCallback('Elasticsearch cluster state did not become yellow');
-                });
-        };
-
-        this.client.indices.create({index: index}).then(() => setTimeout(handleClose, 1000));
-        // handleSettings();
-
-    }
-
-    /**
-     * Send all collected bulk data if any.
-     *
-     * @param {boolean=} closeAfterBulk
-     */
-    sendBulkData(closeAfterBulk?): Promise<BulkResponse> {
-        if (this._bulkData) {
-            log.debug('Sending UrlCheck data to index ' + this.indexName);
-            let promise = this.bulk(this._bulkData, closeAfterBulk);
-            this._bulkData = [];
-            return promise;
+        catch(err) {
+            let message = 'Error occurred creating UrlCheck index';
+            log.error(message, err);
         }
-        return new Promise(resolve => resolve({
-            queued: true
-        }));
-    }
-
-    /**
-     * Index data in batches
-     * @param {object} data
-     * @param {boolean} closeAfterBulk
-     */
-    bulk(data, closeAfterBulk): Promise<BulkResponse> {
-        return new Promise((resolve, reject) => {
-            try {
-                this.client.bulk({
-                    index: this.indexName,
-                    // type: this.settings.indexType || 'base',
-                    body: data
-                })
-                    .then(response => {
-                        if (response.errors) {
-                            response.items.forEach(item => {
-                                let err = item.index.error;
-                                if (err) {
-                                    log.error(`Error during indexing on index '${this.indexName}' for item.id '${item.index._id}': ${JSON.stringify(err)}`, err);
-                                }
-                            });
-                        }
-                        if (closeAfterBulk) {
-                            log.debug('Closing client connection to Elasticsearch');
-                            this.client.close();
-                        }
-                        log.debug('Bulk finished of data #items: ' + data.length / 2);
-                        resolve({
-                            queued: false,
-                            response: response
-                        });
-                    })
-                    .catch(err => {
-                        log.error('Error occurred during bulk index of #items: ' + data.length / 2, err);
-                        if (closeAfterBulk) {
-                            this.client.close();
-                        }
-                        reject(err);
-                    });
-            } catch (e) {
-                log.error('Error during bulk indexing of #items: ' + data.length / 2, e);
-            }
-        });
     }
 }
--- server/app/statistic/statistic.utils.ts
+++ server/app/statistic/statistic.utils.ts
@@ -22,49 +22,29 @@
  */
 
 import {Summary} from '../model/summary';
-import {ElasticSettings} from '../utils/elastic.setting';
-import {Index} from '@shared/index.model';
 import {elasticsearchMapping} from "./statistic.mapping";
 import {elasticsearchSettings} from "./statistic.settings";
 import {ImportLogMessage} from "../model/import.result";
 import { MiscUtils } from '../utils/misc.utils';
 import { Client } from '@elastic/elasticsearch';
+import { ElasticSearchUtils } from '../utils/elastic.utils';
 
 let log = require('log4js').getLogger(__filename);
 
 require('url').URL;
 
-export interface BulkResponse {
-    queued: boolean;
-    response?: any;
-}
-
-export class StatisticUtils {
+export class StatisticUtils extends ElasticSearchUtils {
     public static maxBulkSize = 100;
 
-    private static readonly LENGTH_OF_TIMESTAMP = 18;
-
-    settings: ElasticSettings;
-    client: Client;
-    _bulkData: any[];
-    indexName: string;
-    deduplicationAlias: string;
-
     constructor(settings) {
-        this.settings = settings;
-
-        // the elasticsearch client for accessing the cluster
-        this.client = new Client({
-            node: this.settings.elasticSearchUrl,
-            auth: {
-                username: 'elastic',
-                password: this.settings.elasticSearchPassword
-            },
-            requestTimeout: 30000
-        });
-        this._bulkData = [];
-        this.indexName = "mcloud_harvester_statistic"
-        this.deduplicationAlias = this.settings.deduplicationAlias;
+        // let generalSettings = ConfigService.getGeneralSettings();
+        settings = {
+            ...settings,
+            index: 'mcloud_harvester_statistic'
+        };
+        // @ts-ignore
+        const summary: Summary = {};
+        super(settings, summary);
     }
 
     async saveSummary(logMessage: ImportLogMessage, baseIndex: string){
@@ -89,14 +69,16 @@ export class StatisticUtils {
                 duration: logMessage.duration,
                 warnings: Array.from(warnings.entries()).map(entry => {return {message: entry[0], count: entry[1]}}),
                 errors: Array.from(errors.entries()).map(entry => {return {message: entry[0], count: entry[1]}})
-        }, baseIndex+"_"+timestamp.toISOString());
+        }, baseIndex+"_"+timestamp.toISOString(), StatisticUtils.maxBulkSize);
 
-        await this.prepareIndex(elasticsearchMapping, elasticsearchSettings)
-            .then(() => this.finishIndex())
-            .catch(err => {
-                let message = 'Error occurred creating statistic index';
-                log.error(message, err);
-            });
+        try {
+            await this.prepareIndex(elasticsearchMapping, elasticsearchSettings, true)
+            await this.finishIndex(false);
+        }
+        catch(err) {
+            let message = 'Error occurred creating statistic index';
+            log.error(message, err);
+        }
     }
 
     collectErrorsOrWarnings(result: Map<string, number>, messages: string[]){
@@ -110,221 +92,5 @@ export class StatisticUtils {
             else
                 result.set(truncatedMessage, 1);
         })
-    }
-
-    /**
-     *
-     * @param mapping
-     * @param settings
-     */
-    prepareIndex(mapping, settings) {
-        let idxSettings = {
-            number_of_shards: this.settings.numberOfShards,
-            number_of_replicas: this.settings.numberOfReplicas
-        }
-        return new Promise<void>((resolve, reject) => {
-            this.isIndexPresent(this.indexName).then((isPresent) => {
-
-                if (!isPresent) {
-                    this.client.indices.create({index: this.indexName, wait_for_active_shards: 1, settings: idxSettings})
-                        .then(() => this.addMapping(this.indexName, this.settings.indexType, mapping, settings, resolve, reject))
-                        .catch(err => {
-                            let message = 'Error occurred creating statistic index';
-                            log.error(message, err);
-                            reject(message);
-                        });
-                } else {
-                    this.client.indices.open({index: this.indexName, wait_for_active_shards: 1}).catch(err => {
-                        let message = 'Error occurred creating statistic index';
-                        log.error(message, err);
-                        reject(message);
-                    });
-                    resolve();
-                }
-            });
-        });
-    }
-
-    finishIndex() {
-        return this.client.cluster.health({wait_for_status: 'yellow'})
-            .then(() =>
-                this.sendBulkData(false))
-            .then(() => {
-                log.info('Successfully added statistic data into index: ' + this.indexName);
-            })
-            .catch(err => log.error('Error finishing index', err));
-    }
-
-
-    /**
-     * Add the specified alias to an index.
-     *
-     * @param {string} index
-     * @param {string} alias
-     */
-    async addAlias(index, alias): Promise<any> {
-        return await this.client.indices.putAlias({
-            index: index,
-            name: alias
-        });
-    }
-
-    isIndexPresent(index: string){
-        let result: boolean;
-        return this.client.cat.indices({
-            h: ['index'],
-            format: 'json'
-        }).then(response => {
-            return response
-                .some(json => {
-                    return index === json.index;
-                })
-        }).catch(e => log.error(e));
-    }
-
-    /**
-     * Add the specified mapping to an index and type.
-     *
-     * @param {string} index
-     * @param {string} type
-     * @param {object} mapping
-     * @param {object} settings
-     * @param callback
-     * @param errorCallback
-     */
-    addMapping(index, type, mapping, settings, callback, errorCallback) {
-
-        // set settings
-        const handleSettings = () => {
-            this.client.indices.putSettings({
-                index: index,
-                body: settings
-            }).catch(err => {
-                if (err) {
-                    log.error('Error occurred adding settings', err);
-                    errorCallback(err);
-                } else handleMapping(); //this.client.indices.open({ index: index });
-            });
-        };
-
-        // set mapping
-        const handleMapping = () => {
-            this.client.indices.putMapping({
-                index: index,
-                // type: type || 'base',
-                body: mapping
-            }).catch(err => {
-                if (err) {
-                    log.error('Error occurred adding mapping', err);
-                    errorCallback('Mapping error');
-                } else this.client.indices.open({index: index}).catch(errOpen => {
-                    if (errOpen) {
-                        log.error('Error opening index', errOpen);
-                    }
-                    callback();
-                });
-            });
-        };
-
-        // in order to update settings the index has to be closed
-        const handleClose = () => {
-            this.client.cluster.health({wait_for_status: 'yellow'})
-                .then(() => this.client.indices.close({index: index}).then(handleSettings))
-                .catch(() => {
-                    log.error('Cluster state did not become yellow');
-                    errorCallback('Elasticsearch cluster state did not become yellow');
-                });
-        };
-
-        this.client.indices.create({index: index}).then(() => setTimeout(handleClose, 1000));
-        // handleSettings();
-
-    }
-
-    /**
-     * Index data in batches
-     * @param {object} data
-     * @param {boolean} closeAfterBulk
-     */
-    bulk(data, closeAfterBulk): Promise<BulkResponse> {
-        return new Promise((resolve, reject) => {
-            try {
-                this.client.bulk({
-                    index: this.indexName,
-                    // type: this.settings.indexType || 'base',
-                    body: data
-                })
-                    .then(response => {
-                        if (response.errors) {
-                            response.items.forEach(item => {
-                                let err = item.index.error;
-                                if (err) {
-                                    log.error(`Error during indexing on index '${this.indexName}' for item.id '${item.index._id}': ${JSON.stringify(err)}`, err);
-                                }
-                            });
-                        }
-                        if (closeAfterBulk) {
-                            log.debug('Closing client connection to Elasticsearch');
-                            this.client.close();
-                        }
-                        log.debug('Bulk finished of data #items: ' + data.length / 2);
-                        resolve({
-                            queued: false,
-                            response: response
-                        });
-                    })
-                    .catch(err => {
-                        log.error('Error occurred during bulk index of #items: ' + data.length / 2, err);
-                        if (closeAfterBulk) {
-                            this.client.close();
-                        }
-                        reject(err);
-                    });
-            } catch (e) {
-                log.error('Error during bulk indexing of #items: ' + data.length / 2, e);
-            }
-        });
-    }
-
-
-    /**
-     * Send all collected bulk data if any.
-     *
-     * @param {boolean=} closeAfterBulk
-     */
-    sendBulkData(closeAfterBulk?): Promise<BulkResponse> {
-        if (this._bulkData) {
-            log.debug('Sending statistic data to index ' + this.indexName);
-            let promise = this.bulk(this._bulkData, closeAfterBulk);
-            this._bulkData = [];
-            return promise;
-        }
-        return new Promise(resolve => resolve({
-            queued: true
-        }));
-    }
-
-
-    /**
-     * Add a document to the bulk array which will be sent to the elasticsearch node
-     * if a certain limit {{maxBulkSize}} is reached.
-     * @param doc
-     * @param {string|number} id
-     */
-    addDocToBulk(doc, id): Promise<BulkResponse> {
-        this._bulkData.push({
-            index: {
-                _id: id
-            }
-        });
-        this._bulkData.push(doc);
-
-            return new Promise(resolve => resolve({
-                queued: true
-            }));
-    }
-
-    async search(indexName: string): Promise<any> {
-        return await this.client.search({ index: indexName });
     }
 }
