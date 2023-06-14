61f5b6bce009f9e36bcbea6ec982bfbd0440c8c3
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2023-06-14 11:55:35 +0200

    Don't wait for client each time a doc is added

--- server/app/utils/elastic.utils.7.ts
+++ server/app/utils/elastic.utils.7.ts
@@ -296,7 +296,7 @@ export class ElasticSearchUtils7 extends ElasticSearchUtils {
 
         // send data to elasticsearch if limit is reached
         // TODO: don't use document size but bytes instead
-        await this.client.cluster.health({wait_for_status: 'yellow'});
+        // await this.client.cluster.health({wait_for_status: 'yellow'});
         if (this._bulkData.length >= (maxBulkSize * 2)) {
             return this.sendBulkData();
         } else {
