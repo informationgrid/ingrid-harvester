b705763651e03b3bde88fede873ea1b51b6ab0b0
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2023-08-02 15:00:22 +0200

    DIPLAN: Rework bucket query

--- server/app/persistence/postgres.queries.ts
+++ server/app/persistence/postgres.queries.ts
@@ -48,17 +48,51 @@ export class PostgresQueries {
 
     static readDatasets = `SELECT dataset FROM public.${PostgresQueries.tableName}`;
 
-    static getBuckets = (source: string) => `SELECT A.id as primary_id, B.id as id, B.identifier as identifier,
-        B.dataset as dataset, A.id = B.id as is_primary,
-        (A.dataset->>'alternateTitle' = B.dataset->>'alternateTitle' AND A.id != B.id) as is_duplicate,
-        B.dataset->'extras'->'operates_on' ? A.identifier as is_operating_service
-        FROM ${PostgresQueries.tableName} as A
-        LEFT JOIN ${PostgresQueries.tableName} as B
-        ON (
-            A.dataset->>'alternateTitle' = B.dataset->>'alternateTitle'
-            OR B.dataset->'extras'->'operates_on' ? A.identifier
-        )
-        WHERE A.source = '${source}'
-            AND A.dataset->'extras'->>'hierarchy_level' = 'dataset'
-        ORDER BY primary_id`;
+    /**
+     * Create a query for retrieving all items for a given source.
+     * 
+     * With each result row representing an item, this query should expose the following columns:
+     * - id: the ID of the item
+     * - primary_id: the ID of the dataset this item is a duplicate of, or is a service to
+     * - is_primary: true, if this item is the primary dataset
+     * - is_duplicate: true, if this item is a duplicate of the primary dataset
+     * - is_service: true, if this item is a service to the primary dataset
+     * - dataset: the dataset document of this item
+     * 
+     * @param source the source of the requested items
+     * @returns a database query to return grouped (by `primary_id`) items of rows representing items
+     */
+    static getBuckets = (source: string) =>
+        `SELECT 
+            COALESCE(coupled.service_id, deduplicated.duplicate_id) AS id,
+            deduplicated.primary_id AS primary_id,
+            deduplicated.is_primary AS is_primary,
+            (not deduplicated.is_primary) AS is_duplicate,
+            COALESCE(coupled.is_service, false) AS is_service,
+            COALESCE(coupled.dataset, deduplicated.dataset) AS dataset
+        FROM (
+            SELECT prim.id AS primary_id, duplicate.id AS duplicate_id,
+                prim.identifier AS prim_identifier, duplicate.identifier AS dupl_identifier,
+                duplicate.id = prim.id AS is_primary,
+                duplicate.dataset AS dataset
+            FROM public.${PostgresQueries.tableName} AS prim
+            LEFT JOIN public.${PostgresQueries.tableName} AS duplicate
+            ON prim.dataset->>'alternateTitle' = duplicate.dataset->>'alternateTitle'
+            WHERE prim.source = '${source}'
+                AND (prim.source != duplicate.source OR prim.id = duplicate.id)
+        ) AS deduplicated
+        LEFT JOIN (
+            SELECT ds.id AS dataset_id, service.id AS service_id, ds.identifier AS dataset_identifier,
+                service.dataset AS dataset,
+                service.dataset->'extras'->'operates_on' AS OPERATES,
+                true AS is_service
+            FROM public.${PostgresQueries.tableName} AS ds
+            LEFT JOIN public.${PostgresQueries.tableName} AS service
+            ON ds.identifier = ANY(service.operates_on)
+            WHERE ds.source = '${source}'
+                AND service.dataset->'extras'->>'hierarchy_level' = 'service'
+        ) AS coupled
+        ON
+            deduplicated.primary_id = coupled.dataset_id
+        ORDER BY deduplicated.primary_id`;
 }
