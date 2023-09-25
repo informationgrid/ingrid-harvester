5d4b5db14c009aab87f371d47de2dbbad12a90db
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2023-09-25 11:36:07 +0200

    Fix UUID identification

--- server/app/utils/misc.utils.ts
+++ server/app/utils/misc.utils.ts
@@ -101,7 +101,7 @@ export class MiscUtils {
         if (s == null) {
             return false;
         }
-        return /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/i.test(s);
+        return /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(s);
     }
 
     /**
