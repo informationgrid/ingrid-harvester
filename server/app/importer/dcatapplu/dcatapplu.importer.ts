06b9f574512e8ca4391e683b3f320216ec875b46
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2023-10-31 11:52:17 +0100

    Adapt dcatapplu harvester to BeteiligungsDB output (json)

--- server/app/importer/dcatapplu/dcatapplu.importer.ts
+++ server/app/importer/dcatapplu/dcatapplu.importer.ts
@@ -119,161 +119,165 @@ export class DcatappluImporter extends Importer {
     }
 
     async harvest() {
-        let retries = 0;
+        // let retries = 0;
 
-        while (true) {
+        // while (true) {
             log.debug('Requesting next records');
             let response = await this.requestDelegate.doRequest();
             let harvestTime = new Date(Date.now());
 
-            let responseDom = this.domParser.parseFromString(response);
+            // let responseDom = this.domParser.parseFromString(response);
 
-            let isLastPage = false;
+            // let isLastPage = false;
 
-            let pagedCollection = responseDom.getElementsByTagNameNS(namespaces.HYDRA, 'PagedCollection')[0];
-            if (pagedCollection) {
-                retries = 0;
+            // let pagedCollection = responseDom.getElementsByTagNameNS(namespaces.HYDRA, 'PagedCollection')[0];
+            // if (pagedCollection) {
+            //     retries = 0;
 
-                let numReturned = responseDom.getElementsByTagNameNS(namespaces.DCAT, 'Dataset').length;
-                let itemsPerPage = DcatappluMapper.select('./hydra:itemsPerPage', pagedCollection, true).textContent;
-                this.totalRecords = DcatappluMapper.select('./hydra:totalItems', pagedCollection, true).textContent;
+            //     let numReturned = responseDom.getElementsByTagNameNS(namespaces.DCAT, 'Dataset').length;
+            //     let itemsPerPage = DcatappluMapper.select('./hydra:itemsPerPage', pagedCollection, true).textContent;
+            //     this.totalRecords = DcatappluMapper.select('./hydra:totalItems', pagedCollection, true).textContent;
 
-                let thisPageUrl = pagedCollection.getAttribute('rdf:about');
+            //     let thisPageUrl = pagedCollection.getAttribute('rdf:about');
 
-                let thisPage = Number(DcatappluImporter.getPageFromUrl(thisPageUrl));
+            //     let thisPage = Number(DcatappluImporter.getPageFromUrl(thisPageUrl));
 
-                let lastPage = this.totalRecords/itemsPerPage;
-                let lastPageUrlElement = DcatappluMapper.select('./hydra:lastPage', pagedCollection, true);
-                if(lastPageUrlElement){
-                    let lastPageUrl = lastPageUrlElement.textContent;
-                    lastPage = Number(DcatappluImporter.getPageFromUrl(lastPageUrl));
-                }
+            //     let lastPage = this.totalRecords/itemsPerPage;
+            //     let lastPageUrlElement = DcatappluMapper.select('./hydra:lastPage', pagedCollection, true);
+            //     if(lastPageUrlElement){
+            //         let lastPageUrl = lastPageUrlElement.textContent;
+            //         lastPage = Number(DcatappluImporter.getPageFromUrl(lastPageUrl));
+            //     }
 
 
-                isLastPage = thisPage >= lastPage;
-                if(!isLastPage){
-                    let nextPageUrl = DcatappluMapper.select('./hydra:nextPage', pagedCollection, true).textContent;
-                    let nextPage = Number(DcatappluImporter.getPageFromUrl(nextPageUrl));
-                    this.requestDelegate.updateConfig({qs: {page: nextPage}});
-                }
+            //     isLastPage = thisPage >= lastPage;
+            //     if(!isLastPage){
+            //         let nextPageUrl = DcatappluMapper.select('./hydra:nextPage', pagedCollection, true).textContent;
+            //         let nextPage = Number(DcatappluImporter.getPageFromUrl(nextPageUrl));
+            //         this.requestDelegate.updateConfig({qs: {page: nextPage}});
+            //     }
 
-                log.debug(`Received ${numReturned} records from ${this.settings.catalogUrl} - Page: ${thisPage}`);
-                await this.extractRecords(response, harvestTime)
-            }
-            else {
-                let numReturned = responseDom.getElementsByTagNameNS(namespaces.DCAT, 'Dataset').length;
-                if(numReturned > 0){
-                    await this.extractRecords(response, harvestTime);
-                    isLastPage = true;
-                } else {
-                    const message = `Error while fetching DCAT Records. Will continue to try and fetch next records, if any.\nServer response: ${MiscUtils.truncateErrorMessage(responseDom.toString())}.`;
-                    log.error(message);
-                    this.summary.appErrors.push(message);
-                    if(retries++ > 3){
-                        isLastPage = true;
-                        log.error('Stopped after 3 Retries')
-                    }
-                }
-            }
-
-            if (isLastPage) break;
-        }
+            //     log.debug(`Received ${numReturned} records from ${this.settings.catalogUrl} - Page: ${thisPage}`);
+                await this.extractRecords(response, harvestTime);
+            // }
+            // else {
+            //     let numReturned = responseDom.getElementsByTagNameNS(namespaces.DCAT, 'Dataset').length;
+            //     if(numReturned > 0){
+            //         await this.extractRecords(response, harvestTime);
+            //         isLastPage = true;
+            //     } else {
+            //         const message = `Error while fetching DCAT Records. Will continue to try and fetch next records, if any.\nServer response: ${MiscUtils.truncateErrorMessage(responseDom.toString())}.`;
+            //         log.error(message);
+            //         this.summary.appErrors.push(message);
+            //         if(retries++ > 3){
+            //             isLastPage = true;
+            //             log.error('Stopped after 3 Retries')
+            //         }
+            //     }
+            // }
+            // if (isLastPage) {
+            //     break;
+            // }
+        // }
         // send leftovers
         await this.database.sendBulkData();
     }
 
-    async extractRecords(getRecordsResponse, harvestTime) {
+    async extractRecords(ogcApiResponse, harvestTime) {
         let promises = [];
-        let xml = this.domParser.parseFromString(getRecordsResponse, 'application/xml');
-        let rootNode = xml.getElementsByTagNameNS(namespaces.RDF, 'RDF')[0];
-        // let records =  DcatappluMapper.select('./dcat:Catalog/dcat:dataset/dcat:Dataset|./dcat:Dataset', rootNode);
-        let records =  DcatappluMapper.select('./dcat:Dataset', rootNode);
 
-        let catalogs = DcatappluMapper.select('./dcat:Catalog', rootNode);
-        let catalogAboutsToCatalogs = {};
-        let datasetAboutsToCatalogAbouts = {};
+        let dcatApPluDocuments = ogcApiResponse?.map(obj => obj.dcat_ap_plu);
+        for (let dcatApPluDocument of dcatApPluDocuments) {
+            let xml = this.domParser.parseFromString(dcatApPluDocument, 'application/xml');
+            let rootNode = xml.getElementsByTagNameNS(namespaces.RDF, 'RDF')[0];
+            // let records =  DcatappluMapper.select('./dcat:Catalog/dcat:dataset/dcat:Dataset|./dcat:Dataset', rootNode);
+            let records =  DcatappluMapper.select('./dcat:Dataset', rootNode);
 
-        catalogs.forEach((catalogElement) => {
-            let catalogAbout = DcatappluMapper.select('./@rdf:about', catalogElement, true)?.textContent;
-            let catalog: Catalog = {
-                title:  DcatappluMapper.select('./dct:title', catalogElement, true)?.textContent ?? "",
-                description: DcatappluMapper.select('./dct:description', catalogElement, true)?.textContent ?? "",
-                homepage: DcatappluMapper.select('./foaf:homepage/@rdf:resource', catalogElement, true)?.textContent,
-                identifier: DcatappluMapper.select('./dct:identifier', catalogElement, true)?.textContent,
-                issued: DcatappluMapper.select('./dct:issued', catalogElement, true)?.textContent,
-                language: DcatappluMapper.select('./dct:language/@rdf:resource', catalogElement, true)?.textContent,
-                modified: DcatappluMapper.select('./dct:modified', catalogElement, true)?.textContent,
-                publisher: {
-                    name: DcatappluMapper.select('./dct:publisher/foaf:Agent/foaf:name', catalogElement, true)?.textContent ?? undefined,
-                    type: DcatappluMapper.select('./dct:publisher/foaf:Agent/dct:type', catalogElement, true)?.textContent ?? undefined
-                },
-                themeTaxonomy: DcatappluMapper.select('./dcat:themeTaxonomy/skos:ConceptScheme/dct:title', catalogElement, true)?.textContent,
-                // records?: Record[],
-            };
-            catalogAboutsToCatalogs[catalogAbout] = catalog;
-            let catalogDatasets = DcatappluMapper.select('./dcat:dataset', catalogElement);
-            catalogDatasets.forEach((datasetElement) => {
-                let datasetAbout = DcatappluMapper.select('./@rdf:resource', datasetElement, true)?.textContent;
-                datasetAboutsToCatalogAbouts[datasetAbout] =  catalogAbout
-            });
-        })
+            let catalogs = DcatappluMapper.select('./dcat:Catalog', rootNode);
+            let catalogAboutsToCatalogs = {};
+            let datasetAboutsToCatalogAbouts = {};
 
-        let ids = [];
-        for (let i = 0; i < records.length; i++) {
-            let uuid = DcatappluMapper.select('./dct:identifier', records[i], true).textContent;
-            if(!uuid) {
-                uuid = DcatappluMapper.select('./dct:identifier/@rdf:resource', records[i], true).textContent;
-            }
-            ids.push(uuid);
-        }
-
-        for (let i = 0; i < records.length; i++) {
-            this.summary.numDocs++;
-
-            let uuid = DcatappluMapper.select('./dct:identifier', records[i], true).textContent;
-            if(!uuid) {
-                uuid = DcatappluMapper.select('./dct:identifier/@rdf:resource', records[i], true).textContent;
-            }
-            if (!this.filterUtils.isIdAllowed(uuid)) {
-                this.summary.skippedDocs.push(uuid);
-                continue;
-            }
-
-            if (log.isDebugEnabled()) {
-                log.debug(`Import document ${i + 1} from ${records.length}`);
-            }
-            if (logRequest.isDebugEnabled()) {
-                logRequest.debug("Record content: ", records[i].toString());
-            }
-
-            let rdfAboutAttribute = DcatappluMapper.select('./@rdf:about', records[i], true)?.textContent;
-            let catalogId = datasetAboutsToCatalogAbouts[rdfAboutAttribute];
-            let catalog = catalogAboutsToCatalogs[catalogId];
-            let mapper = this.getMapper(this.settings, records[i], catalog, rootNode, harvestTime, this.summary);
-
-            let doc: any = await this.profile.getIndexDocument().create(mapper).catch(e => {
-                log.error('Error creating index document', e);
-                this.summary.appErrors.push(e.toString());
-                mapper.skipped = true;
-            });
-
-            if (!this.settings.dryRun && !mapper.shouldBeSkipped()) {
-                let entity: Entity = {
-                    identifier: uuid,
-                    source: this.settings.catalogUrl,
-                    collection_id: this.database.defaultCatalog.id,
-                    operates_on: mapper.getOperatesOn(),
-                    dataset: doc,
-                    original_document: mapper.getHarvestedData()
+            catalogs.forEach((catalogElement) => {
+                let catalogAbout = DcatappluMapper.select('./@rdf:about', catalogElement, true)?.textContent;
+                let catalog: Catalog = {
+                    title:  DcatappluMapper.select('./dct:title', catalogElement, true)?.textContent ?? "",
+                    description: DcatappluMapper.select('./dct:description', catalogElement, true)?.textContent ?? "",
+                    homepage: DcatappluMapper.select('./foaf:homepage/@rdf:resource', catalogElement, true)?.textContent,
+                    identifier: DcatappluMapper.select('./dct:identifier', catalogElement, true)?.textContent,
+                    issued: DcatappluMapper.select('./dct:issued', catalogElement, true)?.textContent,
+                    language: DcatappluMapper.select('./dct:language/@rdf:resource', catalogElement, true)?.textContent,
+                    modified: DcatappluMapper.select('./dct:modified', catalogElement, true)?.textContent,
+                    publisher: {
+                        name: DcatappluMapper.select('./dct:publisher/foaf:Agent/foaf:name', catalogElement, true)?.textContent ?? undefined,
+                        type: DcatappluMapper.select('./dct:publisher/foaf:Agent/dct:type', catalogElement, true)?.textContent ?? undefined
+                    },
+                    themeTaxonomy: DcatappluMapper.select('./dcat:themeTaxonomy/skos:ConceptScheme/dct:title', catalogElement, true)?.textContent,
+                    // records?: Record[],
                 };
-                promises.push(this.database.addEntityToBulk(entity));
-            } else {
-                this.summary.skippedDocs.push(uuid);
+                catalogAboutsToCatalogs[catalogAbout] = catalog;
+                let catalogDatasets = DcatappluMapper.select('./dcat:dataset', catalogElement);
+                catalogDatasets.forEach((datasetElement) => {
+                    let datasetAbout = DcatappluMapper.select('./@rdf:resource', datasetElement, true)?.textContent;
+                    datasetAboutsToCatalogAbouts[datasetAbout] =  catalogAbout;
+                });
+            });
+
+            let ids = [];
+            for (let i = 0; i < records.length; i++) {
+                let uuid = DcatappluMapper.select('./dct:identifier', records[i], true).textContent;
+                if(!uuid) {
+                    uuid = DcatappluMapper.select('./dct:identifier/@rdf:resource', records[i], true).textContent;
+                }
+                ids.push(uuid);
             }
-            this.observer.next(ImportResult.running(++this.numIndexDocs, this.totalRecords));
+
+            for (let i = 0; i < records.length; i++) {
+                this.summary.numDocs++;
+
+                let uuid = DcatappluMapper.select('./dct:identifier', records[i], true).textContent;
+                if(!uuid) {
+                    uuid = DcatappluMapper.select('./dct:identifier/@rdf:resource', records[i], true).textContent;
+                }
+                if (!this.filterUtils.isIdAllowed(uuid)) {
+                    this.summary.skippedDocs.push(uuid);
+                    continue;
+                }
+
+                if (log.isDebugEnabled()) {
+                    log.debug(`Import document ${i + 1} from ${records.length}`);
+                }
+                if (logRequest.isDebugEnabled()) {
+                    logRequest.debug("Record content: ", records[i].toString());
+                }
+
+                let rdfAboutAttribute = DcatappluMapper.select('./@rdf:about', records[i], true)?.textContent;
+                let catalogId = datasetAboutsToCatalogAbouts[rdfAboutAttribute];
+                let catalog = catalogAboutsToCatalogs[catalogId] ?? this.database.defaultCatalog;
+                let mapper = this.getMapper(this.settings, records[i], catalog, rootNode, harvestTime, this.summary);
+
+                let doc: any = await this.profile.getIndexDocument().create(mapper).catch(e => {
+                    log.error('Error creating index document', e);
+                    this.summary.appErrors.push(e.toString());
+                    mapper.skipped = true;
+                });
+
+                if (!this.settings.dryRun && !mapper.shouldBeSkipped()) {
+                    let entity: Entity = {
+                        identifier: uuid,
+                        source: this.settings.catalogUrl,
+                        collection_id: this.database.defaultCatalog.id,
+                        operates_on: mapper.getOperatesOn(),
+                        dataset: doc,
+                        original_document: mapper.getHarvestedData()
+                    };
+                    promises.push(this.database.addEntityToBulk(entity));
+                } else {
+                    this.summary.skippedDocs.push(uuid);
+                }
+                this.observer.next(ImportResult.running(++this.numIndexDocs, this.totalRecords));
+            }
         }
-        await Promise.all(promises)
-            .catch(err => log.error('Error indexing DCAT record', err));
+        await Promise.all(promises).catch(err => log.error('Error indexing DCAT record', err));
     }
 
     getMapper(settings, record, catalog, catalogPage, harvestTime, summary): DcatappluMapper {
@@ -284,7 +288,7 @@ export class DcatappluImporter extends Importer {
         let requestConfig: RequestOptions = {
             method: "GET",
             uri: settings.catalogUrl,
-            json: false,
+            json: true,
             proxy: settings.proxy || null
         };
 /*
@@ -308,10 +312,10 @@ export class DcatappluImporter extends Importer {
     }
 
     private static getPageFromUrl(url: string) {
-        let pos = url.indexOf('page=')
+        let pos = url.indexOf('page=');
         if (pos !== -1) {
             url = url.substr(pos + 5);
-            let endPos = url.indexOf('&')
+            let endPos = url.indexOf('&');
             if(endPos > -1){
                 url = url.substr(0, endPos);
             }
