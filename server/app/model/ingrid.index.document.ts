52e7c028f7bcf6a405db21ecdc824954c5f86b08
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2024-06-27 11:22:11 +0200

    Add Ingrid index document (WIP)

---
+++ server/app/model/ingrid.index.document.ts
@@ -0,0 +1,68 @@
+/*
+ * ==================================================
+ * ingrid-harvester
+ * ==================================================
+ * Copyright (C) 2017 - 2024 wemove digital solutions GmbH
+ * ==================================================
+ * Licensed under the EUPL, Version 1.2 or - as soon they will be
+ * approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ * ==================================================
+ */
+
+import { DateRange } from './dateRange';
+import { Geometries, GeometryCollection, Point } from '@turf/helpers';
+
+export type IngridIndexDocument = {
+    id: string,
+    schema_version: string,
+    title: string,
+    abstract?: string,
+    spatial?: Spatial,
+    temporal: {
+        modified: Date,
+        issued: Date,
+        data_temporal?: {
+            date_range: DateRange,
+            date_type?: string
+        }
+    },
+    keyword?: Keyword[],
+    fulltext: string,
+    metadata: Metadata
+};
+
+export type Spatial = {
+    geometry?: Geometries | GeometryCollection,
+    bbox?: Geometries | GeometryCollection,
+    centroid?: Point,
+    inside_point?: Point,
+    location_points?: Point,
+    outline?: Geometries | GeometryCollection,
+    title?: string
+};
+
+export type Keyword = {
+    term: string,
+    type?: string,
+    url?: string
+}
+
+export type Metadata = {
+    issued: Date,
+    modified: Date,
+    source: {
+
+    }
+};
