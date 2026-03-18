07106644c309fea9e22d756f1393199187f5f56b
aawemove <armin.arndt@wemove.com>
2026-03-18 14:27:16 +0100

    Parallelize import socket with env variable THREADPOOL_SIZE

--- server/app/sockets/import.socket.service.ts
+++ server/app/sockets/import.socket.service.ts
@@ -24,6 +24,7 @@
 import { Emit, Input, Namespace, Nsp, Socket, SocketService, SocketSession } from '@tsed/socketio';
 import { CronJob } from 'cron';
 import log4js from 'log4js';
+import pLimit from 'p-limit';
 import * as SocketIO from 'socket.io';
 import type { ImportLogMessage } from '../model/import.result.js';
 import { ElasticsearchFactory } from '../persistence/elastic.factory.js';
@@ -39,6 +40,8 @@ export class ImportSocketService {
     @Nsp nsp: Namespace;
 
     log = log4js.getLogger();
+
+    private limit = pLimit(ConfigService.getThreadpoolSize());
 
     constructor(private summaryService: SummaryService) {
     }
@@ -60,77 +63,89 @@ export class ImportSocketService {
     @Input('runImport')
     @Emit('/log')
     async runImport(id: number, isIncremental?: boolean): Promise<void> {
-        try {
-            let lastExecution = new Date();
-            let configGeneral = ConfigService.getGeneralSettings();
-            let configData = ConfigService.get().filter(config => config.id === id)[0];
-            //configData.deduplicationAlias = configData.index + 'dedup';
+        return this.limit(() => new Promise<void>((resolve) => {
+            try {
+                let lastExecution = new Date();
+                let configGeneral = ConfigService.getGeneralSettings();
+                let configData = ConfigService.get().filter(config => config.id === id)[0];
+                //configData.deduplicationAlias = configData.index + 'dedup';
 
-            let configHarvester = MiscUtils.merge(configData, configGeneral, { isIncremental });
+                let configHarvester = MiscUtils.merge(configData, configGeneral, { isIncremental });
 
-            let profile = ProfileFactoryLoader.get();
-            // TODO legacy - remove
-            if (profile.useIndexPerCatalog()) {
-                profile.createCatalogIfNotExist(configHarvester.catalogId);
-            }
-            let importer = await profile.getImporter(configHarvester);
-            let mode = isIncremental ? 'incr' : 'full';
-            this.log.info(`>> Running importer: [${configHarvester.type}] ${configHarvester.description}`);
-
-            importer.run.subscribe(response => {
-                response.id = id;
-                response.lastExecution = lastExecution;
-                if (configHarvester.cron?.[mode]?.active) {
-                    response.nextExecution = new CronJob(configHarvester.cron[mode].pattern, () => {
-                    }).nextDate().toDate();
+                let profile = ProfileFactoryLoader.get();
+                // TODO legacy - remove
+                if (profile.useIndexPerCatalog()) {
+                    profile.createCatalogIfNotExist(configHarvester.catalogId);
                 }
-                response.duration = (new Date().getTime() - lastExecution.getTime()) / 1000;
-                this.nsp.emit('/log', response);
+                profile.getImporter(configHarvester).then(importer => {
+                    let mode = isIncremental ? 'incr' : 'full';
+                    this.log.info(`>> Running importer: [${configHarvester.type}] ${configHarvester.description}`);
 
-                // when complete then write information log to file
-                if (response.complete) {
-                    // save old summary to compare
-                    let summaryLastRun: ImportLogMessage = this.summaryService.get(id);
-
-                    // TODO this must be done for each catalog - by the importer?
-                    importer.getSummary().print(this.log);
-                    this.summaryService.update(response);
-                    let statisticUtils = new StatisticUtils(configGeneral);
-                    let elastic = ElasticsearchFactory.getElasticUtils(configGeneral.elasticsearch, null);
-                    let index = profile.useIndexPerCatalog() ? configHarvester.catalogId : elastic.indexName;
-                    statisticUtils.saveSummary(response, index);
-
-                    // when less results send mail
-                    if (!isIncremental && configGeneral.mail.enabled && configGeneral.harvesting.mail.enabled) {
-                        let importedLastRun = (summaryLastRun) ? summaryLastRun.summary.numDocs - summaryLastRun.summary.skippedDocs.length : 0;
-                        let imported = importer.getSummary().numDocs - importer.getSummary().skippedDocs.length;
-                        let diff = configGeneral.harvesting.mail.minDifference ?? 10;
-                        if (importedLastRun * (100 - diff) / 100 >= imported) {
-                            let subject: string;
-                            if (imported === 0)
-                                subject = `Importer [${configHarvester.type}] "${configData.description}" ohne Ergebnisse!`;
-                            else
-                                subject = `Importer [${configHarvester.type}] "${configData.description}" mit weniger Ergebnissen!`;
-                            let text = `Current Run:\n`
-                                + importer.getSummary().toString();
-                            if (summaryLastRun) {
-                                text += `\n\n`
-                                    + `Last Run (`+summaryLastRun.lastExecution+`):\n`
-                                    + summaryLastRun.summary.toString();
+                    importer.run.subscribe({
+                        next: response => {
+                            response.id = id;
+                            response.lastExecution = lastExecution;
+                            if (configHarvester.cron?.[mode]?.active) {
+                                response.nextExecution = new CronJob(configHarvester.cron[mode].pattern, () => {
+                                }).nextDate().toDate();
                             }
-                            MailServer.getInstance().send(subject, text);
-                        }
-                    }
-                }
-            }, error => {
-                this.log.error('There was an error: ', error);
-                if (configGeneral.mail.enabled) {
-                    MailServer.getInstance().send(`Importer [${configHarvester.type}] ${configData.description} failed`, error.toString());
-                }
-            });
-        }
-        catch (e) {
-            this.log.error(`An error occured while harvesting (id=${id}): `, e);
-        }
+                            response.duration = (new Date().getTime() - lastExecution.getTime()) / 1000;
+                            this.nsp.emit('/log', response);
+
+                            // when complete then write information log to file
+                            if (response.complete) {
+                                // save old summary to compare
+                                let summaryLastRun: ImportLogMessage = this.summaryService.get(id);
+
+                                // TODO this must be done for each catalog - by the importer?
+                                importer.getSummary().print(this.log);
+                                this.summaryService.update(response);
+                                let statisticUtils = new StatisticUtils(configGeneral);
+                                let elastic = ElasticsearchFactory.getElasticUtils(configGeneral.elasticsearch, null);
+                                let index = profile.useIndexPerCatalog() ? configHarvester.catalogId : elastic.indexName;
+                                statisticUtils.saveSummary(response, index);
+
+                                // when less results send mail
+                                if (!isIncremental && configGeneral.mail.enabled && configGeneral.harvesting.mail.enabled) {
+                                    let importedLastRun = (summaryLastRun) ? summaryLastRun.summary.numDocs - summaryLastRun.summary.skippedDocs.length : 0;
+                                    let imported = importer.getSummary().numDocs - importer.getSummary().skippedDocs.length;
+                                    let diff = configGeneral.harvesting.mail.minDifference ?? 10;
+                                    if (importedLastRun * (100 - diff) / 100 >= imported) {
+                                        let subject: string;
+                                        if (imported === 0)
+                                            subject = `Importer [${configHarvester.type}] "${configData.description}" ohne Ergebnisse!`;
+                                        else
+                                            subject = `Importer [${configHarvester.type}] "${configData.description}" mit weniger Ergebnissen!`;
+                                        let text = `Current Run:\n`
+                                            + importer.getSummary().toString();
+                                        if (summaryLastRun) {
+                                            text += `\n\n`
+                                                + `Last Run (`+summaryLastRun.lastExecution+`):\n`
+                                                + summaryLastRun.summary.toString();
+                                        }
+                                        MailServer.getInstance().send(subject, text);
+                                    }
+                                }
+                            }
+                        },
+                        error: error => {
+                            this.log.error('There was an error: ', error);
+                            if (configGeneral.mail.enabled) {
+                                MailServer.getInstance().send(`Importer [${configHarvester.type}] ${configData.description} failed`, error.toString());
+                            }
+                            resolve();
+                        },
+                        complete: () => resolve()
+                    });
+                }).catch(e => {
+                    this.log.error(`An error occured while harvesting (id=${id}): `, e);
+                    resolve();
+                });
+            }
+            catch (e) {
+                this.log.error(`An error occured while harvesting (id=${id}): `, e);
+                resolve();
+            }
+        }));
     }
 }
