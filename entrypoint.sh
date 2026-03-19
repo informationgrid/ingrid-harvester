c73e00bb8ca91b551b45e11902b56228da167e6c
André Wallat <andre.wallat@wemove.com>
2026-03-19 10:55:33 +0100

    Configure Keycloak with environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,4 +17,15 @@ if [ -f "$CONFIG_FILE" ]; then
     fi
 fi
 
+# Update keycloak.json with auth-server-url and credentials.secret
+KEYCLOAK_FILE="/opt/ingrid/harvester/keycloak.json"
+if [ -f "$KEYCLOAK_FILE" ]; then
+    if [ -n "$KEYCLOAK_AUTH_SERVER_URL" ]; then
+        sed -i "s@\"auth-server-url\": \".*\"@\"auth-server-url\": \"$KEYCLOAK_AUTH_SERVER_URL\"@" "$KEYCLOAK_FILE"
+    fi
+    if [ -n "$KEYCLOAK_CREDENTIALS_SECRET" ]; then
+        sed -i "s/\"secret\": \".*\"/\"secret\": \"$KEYCLOAK_CREDENTIALS_SECRET\"/" "$KEYCLOAK_FILE"
+    fi
+fi
+
 exec "$@"
