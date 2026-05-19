840e7015b5a18e24e71ee9aa76744ad52543a3e9
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2026-05-19 16:54:32 +0200

    [INGRID] Remove deduplication for ZDM WFS

--- server/app/profiles/ingrid/persistence/queries/getBuckets_zdm.sql
+++ server/app/profiles/ingrid/persistence/queries/getBuckets_zdm.sql
@@ -1,24 +1,15 @@
+-- NO deduplication for ZDM WFS!
 SELECT
-    COALESCE(anchor.{{DATASET_COLUMN}}->>'typename', anchor.id::text) AS anchor_id,
-    secondary.id AS id,
-    secondary.identifier AS identifier,
-    secondary.source AS source,
-    secondary.{{DATASET_COLUMN}} AS dataset,
-    secondary.collection_id AS catalog_id,
+    COALESCE({{DATASET_COLUMN}}->>'typename', id::text) AS anchor_id,
+    id AS id,
+    identifier AS identifier,
+    source AS source,
+    {{DATASET_COLUMN}} AS dataset,
+    collection_id AS catalog_id,
     null AS service_type,
-    secondary.created_on AS issued,
-    secondary.last_modified AS modified,
-    secondary.deleted_on AS deleted
-FROM public.record AS anchor
-INNER JOIN public.record AS secondary
-ON
-    anchor.id = secondary.id
-    OR (
-        anchor.source != secondary.source
-        AND (
-            anchor.identifier = secondary.identifier
-            OR anchor.{{DATASET_COLUMN}}->>'typename' = secondary.{{DATASET_COLUMN}}->>'typename'
-        )
-    )
-WHERE anchor.source = $1
+    created_on AS issued,
+    last_modified AS modified,
+    deleted_on AS deleted
+FROM record
+WHERE source = $1
 ORDER BY anchor_id
