272ff5b1f82f4ef0752aae2956799b51dbd6edef
Erik-Lân Do Dinh <eriklan.dodinh@wemove.com>
2026-09-10 14:30:35 +0200

    Extract base test case into separate module

--- server/test/integration/ingrid.integration.spec.ts
+++ server/test/integration/ingrid.integration.spec.ts
@@ -27,45 +27,14 @@ import { Summary } from '../../app/model/summary.js';
 import { PostgresUtils } from '../../app/persistence/postgres.utils.js';
 import cswEbaConfig from '../data/csw/eba/config.json' with { type: 'json' };
 import cswGdideConfig from '../data/csw/gdide/config.json' with { type: 'json' };
-import { type ImporterIntegrationTestCase, runImporterIntegrationTest, setupIntegrationTestLifecycle } from '../utils/integration-test-runner.js';
+import { runImporterIntegrationTest, setupIntegrationTestLifecycle } from '../utils/integration-test-runner.js';
 import { getTestDatabaseConfig, resetDatabase } from '../utils/postgres-container.js';
+import { cswTestcase } from './base.testcases.js';
 
 describe('Ingrid Integration Tests', function () {
     this.timeout(60000);
 
     setupIntegrationTestLifecycle();
-
-    const cswTestcase = {
-        mocks: [
-            {
-                match: { query: { request: 'GetCapabilities' }},
-                fixture: 'input/GetCapabilities.xml'
-            },
-            {
-                match: { query: { resultType: 'hits' }},
-                fixture: 'input/GetRecordsHits.xml'
-            },
-            {
-                match: { query: { resultType: 'results' }},
-                fixture: 'input/GetRecordsResults.xml'
-            },
-            {
-                match: { method: 'POST', bodyMatch: (body) => body.indexOf('GetCapabilities') !== -1 },
-                fixture: 'input/GetCapabilities.xml'
-            },
-            {
-                match: { method: 'POST', bodyMatch: (body) =>
-                        /<csw:SearchResults numberOfRecordsMatched="\d+" numberOfRecordsReturned="\d+" elementSet="summary" nextRecord="\d+"\/>/.test(body)
-                },
-                fixture: 'input/GetRecordsHits.xml'
-            },
-            {
-                match: { method: 'POST', bodyMatch: (body) => body.indexOf('gmd:MD_Metadata') !== -1 },
-                fixture: 'input/GetRecordsResults.xml'
-            }
-        ],
-        expectedDocsDir: 'elasticsearch'
-    } satisfies Partial<ImporterIntegrationTestCase<any>>;
 
     it('should harvest records from CSW and push them to ES (GDI-DE)', async () => {
         await runImporterIntegrationTest({
